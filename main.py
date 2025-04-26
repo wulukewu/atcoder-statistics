f0de0a4c97c751a3e1d81a6ec2671e2743c523c3
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-04-26 20:55:56 +0800

    chore: remove unused selenium imports

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 
 def update_statics(diff, color, statics):
