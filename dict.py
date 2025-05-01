b5f58059b17c0ad02923e6dfea44003ef05fbe88
yc08 <yencheng.wu.yc@gmail.com>
2025-05-01 21:52:38 +0800

    feat: bugs fixed and a new simple chart

--- dict.py
+++ dict.py
@@ -3,38 +3,62 @@ import requests
 import json
 import os
 # Fetch contest data from AtCoder API
-# contest_problems = requests.get('https://kenkoooo.com/atcoder/resources/contest-problem.json').json()
-# print('contest_problems', contest_problems)
 problem_models = requests.get('https://kenkoooo.com/atcoder/resources/problem-models.json').json()
 # print('problem_models', problem_models)
 merged_problems = requests.get('https://kenkoooo.com/atcoder/resources/merged-problems.json').json()
 # print('merged_problems', merged_problems)
 stats={}
+chart={
+    "abc":{},
+    "arc":{},
+    "agc":{},
+    "others":{}
+}
 for stat in merged_problems:
-    stats[stat["id"]] = {}
-    stats[stat["id"]]["contest_id"]  =stat["contest_id"]
-    stats[stat["id"]]["name"]        =stat["name"]
-    stats[stat["id"]]["point"]       =stat["point"]
-    stats[stat["id"]]["solver_count"]=stat["solver_count"]
+    x="contest_id"
+    if stat[x] not in stats :stats[stat[x]] = {}
+    stats[stat[x]][stat["id"]]={}
+    stats[stat[x]][stat["id"]]["name"]        =stat["name"]
+    stats[stat[x]][stat["id"]]["point"]       =stat["point"]
+    stats[stat[x]][stat["id"]]["solver_count"]=stat["solver_count"]
     if stat["id"] in problem_models:
         if "is_experimental" in problem_models[stat["id"]]:
-            stats[stat["id"]]["is_experimental"]= problem_models[stat["id"]]["is_experimental"]
+            stats[stat[x]][stat["id"]]["is_experimental"]= problem_models[stat["id"]]["is_experimental"]
         if "variance" in problem_models[stat["id"]]:
-            stats[stat["id"]]["variance"]   = problem_models[stat["id"]]["variance"]
+            stats[stat[x]][stat["id"]]["variance"]   = problem_models[stat["id"]]["variance"]
         if "difficulty" in problem_models[stat["id"]]:
-            stats[stat["id"]]["difficulty"] = problem_models[stat["id"]]["difficulty"]
-            if problem_models[stat["id"]]["difficulty"]<400: color="gray"
-            elif problem_models[stat["id"]]["difficulty"]<800: color="brown"
-            elif problem_models[stat["id"]]["difficulty"]<1200: color="green"
-            elif problem_models[stat["id"]]["difficulty"]<1600: color="cyan"
-            elif problem_models[stat["id"]]["difficulty"]<2000: color="blue"
-            elif problem_models[stat["id"]]["difficulty"]<2400: color="yellow"
-            elif problem_models[stat["id"]]["difficulty"]<2800: color="orange"
-            elif problem_models[stat["id"]]["difficulty"]<3200: color="red"
+            stats[stat[x]][stat["id"]]["difficulty"] = problem_models[stat["id"]]["difficulty"]
+            y=problem_models[stat["id"]]["difficulty"]
+            if y<400: color="gray"
+            elif y<800: color="brown"
+            elif y<1200: color="green"
+            elif y<1600: color="cyan"
+            elif y<2000: color="blue"
+            elif y<2400: color="yellow"
+            elif y<2800: color="orange"
+            elif y<3200: color="red"
             else: color="red"
-            stats[stat["id"]]["color"] = color
+            stats[stat[x]][stat["id"]]["color"] = color
+
+for stat in stats:
+    if "abc" in stat:x="abc"
+    elif "arc" in stat:x="arc"
+    elif "agc" in stat:x="agc"
+    else: continue
+    for i in stats[stat].values():
+        if ("color" not in i) or ("point" not in i): continue
+        if i["point"]==None: continue
+        if i["point"] not in chart[x]:chart[x][i["point"]]={}
+        if i["color"] in chart[x][i["point"]]:
+            chart[x][i["point"]][i["color"]]+=1
+        else:
+            chart[x][i["point"]]={i["color"]:1}
+
 os.makedirs('web-page/json', exist_ok=True)
 # Save the stats dictionary to a JSON file
 with open('web-page/json/stats.json', 'w', encoding='utf-8') as f:
     json.dump(stats, f, ensure_ascii=False, indent=2)
 # print(stats)
+with open('web-page/json/chart.json', 'w', encoding='utf-8') as f:
+    json.dump(chart, f, ensure_ascii=False, indent=2)
+# print(chart)
