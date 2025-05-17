a60617553e5918d3e4b28f81eeb7b3e812131d26
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-05-18 00:54:43 +0800

    add StatisticsView component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <statistics-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StatisticsView from './components/StatisticsView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StatisticsView
   }
 }
 </script>
