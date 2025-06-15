8d2e534eb3822b706a8be214b5d6d4211a0610c6
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-06-15 20:21:48 +0800

    try to load static table

--- frontend/src/components/StatisticsView.vue
+++ frontend/src/components/StatisticsView.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <h1>AtCoder Statistics</h1>
+    <h2>Latest Contests: N/A</h2>
+    <atcoder-table />
   </div>
 </template>
 
 <script>
+import AtcoderTable from "./AtcoderTable.vue";
+
 export default {
-  name: 'StatisticsView',
+  name: "StatisticsView",
+  components: {
+    AtcoderTable,
+  },
 };
 </script>
 
-<style scoped>
-
+<style>
+h1 {
+  text-align: center;
+}
 </style>
